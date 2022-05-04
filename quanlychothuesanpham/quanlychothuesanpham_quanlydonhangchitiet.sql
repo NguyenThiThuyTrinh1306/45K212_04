86dbf3689be7e2f80280a99e74ad4c2d56977916
Nguyễn Thị Thủy Trinh <thuytrinh2027@gmail.com>
2022-05-04 14:42:51 +0700

    Final Database

---
+++ quanlychothuesanpham/quanlychothuesanpham_quanlydonhangchitiet.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: quanlychothuesanpham
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `quanlydonhangchitiet`
+--
+
+DROP TABLE IF EXISTS `quanlydonhangchitiet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quanlydonhangchitiet` (
+  `MaDH` int NOT NULL,
+  `MaSP` int NOT NULL,
+  `ThanhTien` float DEFAULT NULL,
+  `SoLuong` int NOT NULL,
+  `ThoiGianThue` int DEFAULT NULL,
+  PRIMARY KEY (`MaDH`,`MaSP`),
+  KEY `MaSP` (`MaSP`),
+  CONSTRAINT `quanlydonhangchitiet_ibfk_1` FOREIGN KEY (`MaDH`) REFERENCES `quanlydonhang` (`MaDH`),
+  CONSTRAINT `quanlydonhangchitiet_ibfk_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quanlydonhangchitiet`
+--
+
+LOCK TABLES `quanlydonhangchitiet` WRITE;
+/*!40000 ALTER TABLE `quanlydonhangchitiet` DISABLE KEYS */;
+INSERT INTO `quanlydonhangchitiet` VALUES (30,2,150000,1,1),(30,8,60000,2,3),(31,13,115000,2,6),(32,2,315000,2,5);
+/*!40000 ALTER TABLE `quanlydonhangchitiet` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-03 20:56:58
